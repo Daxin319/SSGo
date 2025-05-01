ed542d00047d047452f7699e3abc90be19e94275
Daxin319 <lylehigg@gmail.com>
2025-04-30 18:14:15 -0600

    added ability to split images and links

--- src/static/nodes/splitNodesDelimiter.go
+++ src/static/nodes/splitNodesDelimiter.go
@@ -3,11 +3,19 @@ package nodes
 import (
 	"bytes"
 	"errors"
+	"fmt"
+	"regexp"
+	"strings"
 )
 
 func SplitNodesDelimiter(oldNodes []TextNode, delimiter string, textType enum) ([]TextNode, error) {
 	var newNodes []TextNode
 	for _, node := range oldNodes {
+		if node.TextType != Text {
+			newNodes = append(newNodes, node)
+			continue
+		}
+
 		idx := bytes.Index([]byte(node.Text), []byte(delimiter))
 		if idx == -1 {
 			newNodes = append(newNodes, node)
@@ -53,4 +61,136 @@ func SplitNodesDelimiter(oldNodes []TextNode, delimiter string, textType enum) (
 
 	return newNodes, nil
 
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Images and Links Handled Below                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type Img struct {
+	Alt string
+	Url string
+}
+
+var imgReg = regexp.MustCompile(`!\[([^\[\]]*)\]\(([^\(\)]*)\)`)
+
+type Lnk struct {
+	Text string
+	Url  string
+}
+
+var linkReg = regexp.MustCompile(`\[(.*?)\]\((.*?)\)`)
+
+func ExtractImages(s string) []Img {
+	var results []Img
+	matches := imgReg.FindAllStringSubmatch(s, -1)
+	for _, m := range matches {
+		if len(m) == 3 {
+			results = append(results, Img{Alt: m[1], Url: m[2]})
+		}
+	}
+	return results
+}
+
+func ExtractLinks(s string) []Lnk {
+	var results []Lnk
+	matches := linkReg.FindAllStringSubmatchIndex(s, -1)
+
+	for _, m := range matches {
+		start := m[0]
+		// skip if preceded by '!'
+		if start > 0 && s[start-1] == '!' {
+			continue
+		}
+
+		text := s[m[2]:m[3]]
+		url := s[m[4]:m[5]]
+		results = append(results, Lnk{Text: text, Url: url})
+	}
+	return results
+}
+
+func SplitNodesImage(oldNodes []TextNode) ([]TextNode, error) {
+	var newNodes []TextNode
+	for _, node := range oldNodes {
+		if node.TextType != Text {
+			newNodes = append(newNodes, node)
+			continue
+		}
+
+		images := ExtractImages(node.Text)
+		if len(images) == 0 {
+			newNodes = append(newNodes, node)
+			continue
+		}
+
+		for _, image := range images {
+			if len(image.Alt) == 0 || len(image.Url) == 0 {
+				return []TextNode{}, errors.New("Invalid image markdown. Empty alt text or URL")
+			}
+			formatted := fmt.Sprintf("![%s](%s)", image.Alt, image.Url)
+			splitText := strings.Split(node.Text, formatted)
+			if len(splitText[0]) != 0 {
+				newNode := TextNode{
+					Text:     splitText[0],
+					TextType: Text,
+				}
+				newNodes = append(newNodes, newNode)
+			}
+			newNode := TextNode{
+				Text:     image.Alt,
+				TextType: Image,
+				Url:      image.Url,
+			}
+			newNodes = append(newNodes, newNode)
+
+			node.Text = splitText[1]
+		}
+		if len(node.Text) != 0 {
+			newNodes = append(newNodes, node)
+		}
+	}
+	return newNodes, nil
+}
+
+func SplitNodesLink(oldNodes []TextNode) ([]TextNode, error) {
+	var newNodes []TextNode
+	for _, node := range oldNodes {
+		if node.TextType != Text {
+			newNodes = append(newNodes, node)
+			continue
+		}
+
+		links := ExtractLinks(node.Text)
+		if len(links) == 0 {
+			newNodes = append(newNodes, node)
+			continue
+		}
+
+		for _, link := range links {
+			if len(link.Text) == 0 || len(link.Url) == 0 {
+				return []TextNode{}, errors.New("Invalid markdown, empty text or url field.")
+			}
+			formatted := fmt.Sprintf("[%s](%s)", link.Text, link.Url)
+			splitText := strings.Split(node.Text, formatted)
+			if len(splitText[0]) != 0 {
+				newNode := TextNode{
+					Text:     splitText[0],
+					TextType: Text,
+				}
+				newNodes = append(newNodes, newNode)
+			}
+			newNode := TextNode{
+				Text:     link.Text,
+				TextType: Link,
+				Url:      link.Url,
+			}
+			newNodes = append(newNodes, newNode)
+			node.Text = splitText[1]
+		}
+		if len(node.Text) != 0 {
+			newNodes = append(newNodes, node)
+		}
+	}
+	return newNodes, nil
 }
