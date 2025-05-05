53bd870e2836160bff7bed1c9e1b813e0c559df5
Daxin319 <lylehigg@gmail.com>
2025-05-05 16:02:33 -0600

    fixed refactor issue with main package

--- src/copyStaticToPublic.go
+++ src/copyStaticToPublic.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func copyStaticToPublic(path string) error {
+	pDir := path + "/public"
+	_, err := os.Stat(pDir)
+	if err == nil {
+		err = os.RemoveAll(pDir)
+	}
+	sDir := path + "/static"
+	dir := os.DirFS(sDir)
+
+	err = os.CopyFS(pDir, dir)
+	if err != nil {
+		fmt.Println(err)
+		return errors.New("Error copying files")
+	}
+	return nil
+}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 )
@@ -12,26 +11,9 @@ func main() {
 		msg := fmt.Errorf("Error getting working directory: %v", err)
 		fmt.Println(msg)
 	}
-	err = CopyStaticToPublic(path)
+	err = copyStaticToPublic(path)
 	if err != nil {
 		msg := fmt.Errorf("Error copying files: %v", err)
 		fmt.Println(msg)
 	}
-}
-
-func CopyStaticToPublic(path string) error {
-	pDir := path + "/public"
-	_, err := os.Stat(pDir)
-	if err == nil {
-		err = os.RemoveAll(pDir)
-	}
-	sDir := path + "/static"
-	dir := os.DirFS(sDir)
-
-	err = os.CopyFS(pDir, dir)
-	if err != nil {
-		fmt.Println(err)
-		return errors.New("Error copying files")
-	}
-	return nil
 }
